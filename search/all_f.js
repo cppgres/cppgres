f4d42f5a9ff779beb8d295865dbfaf50fb0344f1
yrashk <yrashk@gmail.com>
2025-03-19 15:20:52 +0000

    Deploying to gh-pages from @ cppgres/cppgres@6c316d1533998d920b764f15e2a7054ebfc5cc76 🚀

--- search/all_f.js
+++ search/all_f.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['set_2ehpp_0',['set.hpp',['../set_8hpp.html',1,'']]],
-  ['set_5fiterator_5ftraits_1',['set_iterator_traits',['../structcppgres_1_1set__iterator__traits.html',1,'cppgres']]],
-  ['set_5fname_2',['set_name',['../structcppgres_1_1tuple__descriptor.html#a1f95a8438537115de925dfcf239e0014',1,'cppgres::tuple_descriptor']]],
-  ['set_5ftype_3',['set_type',['../structcppgres_1_1tuple__descriptor.html#af4d09b69d5f90e56a9fff7cc1b0a6047',1,'cppgres::tuple_descriptor']]],
-  ['spi_5fexecutor_4',['spi_executor',['../structcppgres_1_1spi__executor.html',1,'cppgres::spi_executor'],['../structcppgres_1_1spi__executor.html#a726d3a8355cbd06be5aec2b252c6d9b9',1,'cppgres::spi_executor::spi_executor()'],['../structcppgres_1_1spi__nonatomic__executor.html#a726d3a8355cbd06be5aec2b252c6d9b9',1,'cppgres::spi_nonatomic_executor::spi_executor()']]],
-  ['spi_5fnonatomic_5fexecutor_5',['spi_nonatomic_executor',['../structcppgres_1_1spi__nonatomic__executor.html',1,'cppgres']]],
-  ['spi_5fplan_6',['spi_plan',['../structcppgres_1_1spi__plan.html',1,'cppgres']]],
-  ['syscache_7',['syscache',['../structcppgres_1_1syscache.html',1,'cppgres']]],
-  ['syscache_5ftraits_8',['syscache_traits',['../structcppgres_1_1syscache__traits.html',1,'cppgres']]],
-  ['syscache_5ftraits_3c_20form_5fpg_5fproc_20_3e_9',['syscache_traits&lt; Form_pg_proc &gt;',['../structcppgres_1_1syscache__traits_3_01Form__pg__proc_01_4.html',1,'cppgres']]],
-  ['syscache_5ftraits_3c_20form_5fpg_5ftype_20_3e_10',['syscache_traits&lt; Form_pg_type &gt;',['../structcppgres_1_1syscache__traits_3_01Form__pg__type_01_4.html',1,'cppgres']]]
+  ['scoped_5fblocked_5fsignals_0',['scoped_blocked_signals',['../structcppgres_1_1background__worker_1_1scoped__blocked__signals.html',1,'cppgres::background_worker']]],
+  ['scoped_5funblocked_5fsignals_1',['scoped_unblocked_signals',['../structcppgres_1_1background__worker_1_1scoped__unblocked__signals.html',1,'cppgres::background_worker']]],
+  ['set_2ehpp_2',['set.hpp',['../set_8hpp.html',1,'']]],
+  ['set_5fiterator_5ftraits_3',['set_iterator_traits',['../structcppgres_1_1set__iterator__traits.html',1,'cppgres']]],
+  ['set_5fname_4',['set_name',['../structcppgres_1_1tuple__descriptor.html#a1f95a8438537115de925dfcf239e0014',1,'cppgres::tuple_descriptor']]],
+  ['set_5ftype_5',['set_type',['../structcppgres_1_1tuple__descriptor.html#af4d09b69d5f90e56a9fff7cc1b0a6047',1,'cppgres::tuple_descriptor']]],
+  ['spi_5fexecutor_6',['spi_executor',['../structcppgres_1_1spi__executor.html',1,'cppgres::spi_executor'],['../structcppgres_1_1spi__executor.html#a726d3a8355cbd06be5aec2b252c6d9b9',1,'cppgres::spi_executor::spi_executor()'],['../structcppgres_1_1spi__nonatomic__executor.html#a726d3a8355cbd06be5aec2b252c6d9b9',1,'cppgres::spi_nonatomic_executor::spi_executor()']]],
+  ['spi_5fnonatomic_5fexecutor_7',['spi_nonatomic_executor',['../structcppgres_1_1spi__nonatomic__executor.html',1,'cppgres']]],
+  ['spi_5fplan_8',['spi_plan',['../structcppgres_1_1spi__plan.html',1,'cppgres']]],
+  ['syscache_9',['syscache',['../structcppgres_1_1syscache.html',1,'cppgres']]],
+  ['syscache_5ftraits_10',['syscache_traits',['../structcppgres_1_1syscache__traits.html',1,'cppgres']]],
+  ['syscache_5ftraits_3c_20form_5fpg_5fproc_20_3e_11',['syscache_traits&lt; Form_pg_proc &gt;',['../structcppgres_1_1syscache__traits_3_01Form__pg__proc_01_4.html',1,'cppgres']]],
+  ['syscache_5ftraits_3c_20form_5fpg_5ftype_20_3e_12',['syscache_traits&lt; Form_pg_type &gt;',['../structcppgres_1_1syscache__traits_3_01Form__pg__type_01_4.html',1,'cppgres']]]
 ];
