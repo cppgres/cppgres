efe61390f98bc421c520a46ffdae6205f2bae88e
Yurii Rashkovskii <yrashk@gmail.com>
2025-03-20 07:11:11 -0400

    Problem: failure to compile on PG16 and below
    Solution: exclude enum values and consts not available there

--- src/cppgres/backend.hpp
+++ src/cppgres/backend.hpp
@@ -20,14 +20,18 @@ enum type {
   autovac_worker = B_AUTOVAC_WORKER,
   bg_worker = B_BG_WORKER,
   wal_sender = B_WAL_SENDER,
+#if PG_MAJORVERSION_NUM >= 17
   slotsync_worker = B_SLOTSYNC_WORKER,
   standalone_backend = B_STANDALONE_BACKEND,
+#endif
   archiver = B_ARCHIVER,
   bg_writer = B_BG_WRITER,
   checkpointer = B_CHECKPOINTER,
   startup = B_STARTUP,
   wal_receiver = B_WAL_RECEIVER,
+#if PG_MAJORVERSION_NUM >= 17
   wal_summarizer = B_WAL_SUMMARIZER,
+#endif
   wal_writer = B_WAL_WRITER,
   logger = B_LOGGER
 };
