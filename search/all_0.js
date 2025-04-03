43dc507dbcadc6111a27211c77edb91f35ceca68
yrashk <yrashk@gmail.com>
2025-04-03 04:11:20 +0000

    Deploying to gh-pages from @ cppgres/cppgres@cf0f85580b2df7b4ce59494ced41dc4b13d41348 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -4,7 +4,8 @@ var searchData=
   ['all_5ffrom_5fnullable_5fdatum_1',['all_from_nullable_datum',['../structcppgres_1_1all__from__nullable__datum.html',1,'cppgres']]],
   ['alloc_5fset_5fmemory_5fcontext_2',['alloc_set_memory_context',['../structcppgres_1_1alloc__set__memory__context.html',1,'cppgres']]],
   ['always_5fcurrent_5fmemory_5fcontext_3',['always_current_memory_context',['../structcppgres_1_1always__current__memory__context.html',1,'cppgres']]],
-  ['attribute_5fname_4',['attribute_name',['../structcppgres_1_1record.html#a8e2fc8d5e09524b1e094ee47c1997295',1,'cppgres::record']]],
-  ['attribute_5ftype_5',['attribute_type',['../structcppgres_1_1record.html#ae951e1976f07a9f6e3de2f035bb04b35',1,'cppgres::record']]],
-  ['attributes_6',['attributes',['../structcppgres_1_1tuple__descriptor.html#a35dd1d6485a39d51acc1835c02da5a26',1,'cppgres::tuple_descriptor::attributes()'],['../structcppgres_1_1record.html#ac4cb9245dc50072185926b411057eac9',1,'cppgres::record::attributes()']]]
+  ['atexit_4',['atexit',['../structcppgres_1_1backend.html#a37b7653b688b1ca96aeac6ad7472229f',1,'cppgres::backend']]],
+  ['attribute_5fname_5',['attribute_name',['../structcppgres_1_1record.html#a8e2fc8d5e09524b1e094ee47c1997295',1,'cppgres::record']]],
+  ['attribute_5ftype_6',['attribute_type',['../structcppgres_1_1record.html#ae951e1976f07a9f6e3de2f035bb04b35',1,'cppgres::record']]],
+  ['attributes_7',['attributes',['../structcppgres_1_1tuple__descriptor.html#a35dd1d6485a39d51acc1835c02da5a26',1,'cppgres::tuple_descriptor::attributes()'],['../structcppgres_1_1record.html#ac4cb9245dc50072185926b411057eac9',1,'cppgres::record::attributes()']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['attribute_5fname_0',['attribute_name',['../structcppgres_1_1record.html#a8e2fc8d5e09524b1e094ee47c1997295',1,'cppgres::record']]],
-  ['attribute_5ftype_1',['attribute_type',['../structcppgres_1_1record.html#ae951e1976f07a9f6e3de2f035bb04b35',1,'cppgres::record']]],
-  ['attributes_2',['attributes',['../structcppgres_1_1tuple__descriptor.html#a35dd1d6485a39d51acc1835c02da5a26',1,'cppgres::tuple_descriptor::attributes()'],['../structcppgres_1_1record.html#ac4cb9245dc50072185926b411057eac9',1,'cppgres::record::attributes()']]]
+  ['atexit_0',['atexit',['../structcppgres_1_1backend.html#a37b7653b688b1ca96aeac6ad7472229f',1,'cppgres::backend']]],
+  ['attribute_5fname_1',['attribute_name',['../structcppgres_1_1record.html#a8e2fc8d5e09524b1e094ee47c1997295',1,'cppgres::record']]],
+  ['attribute_5ftype_2',['attribute_type',['../structcppgres_1_1record.html#ae951e1976f07a9f6e3de2f035bb04b35',1,'cppgres::record']]],
+  ['attributes_3',['attributes',['../structcppgres_1_1tuple__descriptor.html#a35dd1d6485a39d51acc1835c02da5a26',1,'cppgres::tuple_descriptor::attributes()'],['../structcppgres_1_1record.html#ac4cb9245dc50072185926b411057eac9',1,'cppgres::record::attributes()']]]
 ];
